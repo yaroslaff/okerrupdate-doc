d98b5f35f95577c3aa2f7d063a7bc2f33a816d21
Yaroslav Polyakov <yaroslaff@gmail.com>
2023-04-04 21:17:56 +0700

    no static

--- conf.py
+++ conf.py
@@ -58,7 +58,7 @@ html_theme = 'nature'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 # html_static_path = ['_static']
-html_static_path = ['']
+# html_static_path = ['']
 
 master_doc = 'index'
 exclude_patterns = ['README.md']
